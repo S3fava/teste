bae90085c0bc804e1c04d45f4931cbd13397be09
S3fava <sefava26@gmail.com>
2021-03-04 03:18:54 -0300

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user 0xf540FB2C3efe43683cD8239F04a160949c1e532f.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  eu1.ethermine.org --user 0xf540FB2C3efe43683cD8239F04a160949c1e532f.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
